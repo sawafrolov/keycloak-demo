773fe72461504ff32498815be41d3f15914b6c27
sawafrolov <frolov@devs-vault.com>
2025-06-02 12:44:18 +0400

    Отладка

--- auth-service/build.gradle.kts
+++ auth-service/build.gradle.kts
@@ -12,7 +12,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-security")
-    implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
 }
 
 tasks {
