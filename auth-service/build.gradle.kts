94f8558db8eff0f214a45d189575ba8c40afff2e
sawafrolov <frolov@devs-vault.com>
2025-06-02 12:12:00 +0400

    Конфигурация

--- auth-service/build.gradle.kts
+++ auth-service/build.gradle.kts
@@ -9,20 +9,10 @@ repositories {
 }
 
 dependencies {
-
-    // Spring Boot
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
-
-    // Lombok
-    compileOnly("org.projectlombok:lombok:1.18.38")
-    annotationProcessor("org.projectlombok:lombok:1.18.38")
-
-    // Testing
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.springframework.security:spring-security-test")
 }
 
 tasks {
--- auth-service/src/main/java/com/github/sawafrolov/authservice/configuration/OAuth2LoginConfiguration.java
+++ auth-service/src/main/java/com/github/sawafrolov/authservice/configuration/OAuth2LoginConfiguration.java
@@ -2,17 +2,20 @@ package com.github.sawafrolov.authservice.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableMethodSecurity
-public class SecurityConfiguration {
+@EnableWebSecurity
+public class OAuth2LoginConfiguration {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        // todo
+        http.authorizeHttpRequests(authorizeHttpRequests ->
+                        authorizeHttpRequests.anyRequest().authenticated()
+                ).oauth2Login(Customizer.withDefaults());
         return http.build();
     }
 }
