61574dbcbd1fb7b3ad8a9d01cb33df571905997c
sawafrolov <frolov@devs-vault.com>
2025-06-03 17:54:14 +0400

    Отладка

--- auth-service/src/main/java/com/github/sawafrolov/authservice/controllers/HelloController.java
+++ auth-service/src/main/java/com/github/sawafrolov/authservice/controllers/HelloController.java
@@ -1,12 +1,15 @@
 package com.github.sawafrolov.authservice.controllers;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticatedPrincipal;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.OAuth2AuthenticatedPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/hello")
@@ -14,7 +17,10 @@ public class HelloController {
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public String hello(@AuthenticationPrincipal UserDetails userDetails) {
-        return String.format("Hello, %s!", userDetails.getUsername());
+    public String hello(@AuthenticationPrincipal OAuth2AuthenticatedPrincipal authenticatedPrincipal) {
+        return Optional.ofNullable(authenticatedPrincipal)
+                .map(AuthenticatedPrincipal::getName)
+                .map(username -> String.format("Hello, %s!", username))
+                .orElse("Не удалось передать токен авторизации");
     }
 }
